2b51e045c1ebf5ed153220fc6a067c335739efb3
Aradhya Nepal <aradhya.1221@gmail.com>
2023-05-22 15:43:17 +0545

    Removing all

--- android/app/src/main/kotlin/com/example/riverpod_fake_store_api/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.riverpod_fake_store_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screen/auth/model/model.dart
+++
@@ -1,9 +0,0 @@
-class Person{
-  String name;
-  static final Person _instance=Person._("Hello");
-  Person._(this.name);
-
-  factory Person()=>_instance;
-}
-
-//factory keyword vs factory pattern-> Singeleton pattern
